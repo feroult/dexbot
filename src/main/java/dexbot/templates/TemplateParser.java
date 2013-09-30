78390c2ade41179c2771f736191ae80fcb2f9429
Fernando Ultremare <feroult@gmail.com>
2013-09-30 17:27:01 -0300

    template from resource

--- src/main/java/dexbot/templates/TemplateParser.java
+++ src/main/java/dexbot/templates/TemplateParser.java
@@ -1,7 +1,9 @@
-package dexbot;
+package dexbot.templates;
 
+import java.io.IOException;
 import java.io.StringWriter;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
@@ -9,7 +11,7 @@ public class TemplateParser {
 
 	private String template;
 
-	public TemplateParser(String template) {
+	private TemplateParser(String template) {
 		this.template = template;
 	}
 
@@ -32,4 +34,16 @@ public class TemplateParser {
 		return writer.toString();
 	}
 
+	public static TemplateParser fromResource(String name) {
+		try {
+			String template = FileUtils.readFileToString(FileUtils.toFile(TemplateParser.class.getResource(name)));
+			return new TemplateParser(template);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static TemplateParser fromString(String template) {
+		return new TemplateParser(template);
+	}
 }
--- src/test/java/dexbot/templates/TemplateParserTest.java
+++ src/test/java/dexbot/templates/TemplateParserTest.java
@@ -1,8 +1,10 @@
-package dexbot;
+package dexbot.templates;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import dexbot.templates.TemplateParser;
 
 public class TemplateParserTest {
 
@@ -21,14 +23,22 @@ public class TemplateParserTest {
 
 	@Test
 	public void testTemplateWithoutPlaceholders() {
-		TemplateParser template = new TemplateParser("<tag>hello</tag>");
+		TemplateParser template = TemplateParser.fromString("<tag>hello</tag>");
 		assertEquals("<tag>hello</tag>", template.parse());
 	}
 
 	@Test
 	public void testTemplateWithBeanProperty() {
-		TemplateParser template = new TemplateParser("<tag>$bean.hello</tag>");
+		TemplateParser template = TemplateParser.fromString("<tag>$bean.hello</tag>");
 
+		assertEquals("<tag>olah</tag>", template.parse(new Bean("olah")));
+		assertEquals("<tag>ciao</tag>", template.parse(new Bean("ciao")));
+	}
+	
+	@Test
+	public void testTemplateFromResource() {
+		TemplateParser template = TemplateParser.fromResource("/dexbot/templates/hello.html");
+		
 		assertEquals("<tag>olah</tag>", template.parse(new Bean("olah")));
 		assertEquals("<tag>ciao</tag>", template.parse(new Bean("ciao")));
 	}
