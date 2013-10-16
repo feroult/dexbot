3901122ce22483310e8a556799b83ba97d084265
erickzanardoo <erickzanardoo@gmail.com>
2013-10-16 11:34:42 -0300

    Usando ID, assim como e usado no Java

--- dexbot-admin/services/Base.py
+++ dexbot-admin/services/Base.py
@@ -11,15 +11,19 @@ class BaseCrudService:
         base = json.loads(baseStr);
 
         id_key = key_name=base['id_key'];
-        template_base = db.get(id_key);
+        
+        k = db.Key.from_path('template_base', int(id_key));
+        template_base = db.get(k);
+        
         template_base.base = base['base'];
         template_base.desc = base['desc'];
         template_base.put();
 
     def get(request, response):
         id_key = request.get('id');
-        template_base = db.get(id_key);
-        resp = {'base': template_base.base, 'desc': template_base.desc, 'id_key': str(template_base.key())};
+        k = db.Key.from_path('template_base', int(id_key));
+        template_base = db.get(k);
+        resp = {'base': template_base.base, 'desc': template_base.desc, 'id_key': str(template_base.key().id())};
         response.out.write(json.dumps(resp));
         
     methods = {'get': get, 'put': put}
@@ -30,14 +34,14 @@ class BaseListService:
         baseEntity = template_base(desc=desc)
         baseEntity.put();
         response.headers['Content-Type'] = 'application/json';
-        response.out.write(str(baseEntity.key()));
+        response.out.write(str(baseEntity.key().id()));
 
     def get(request, response):
         q = template_base.all();
         result = [];
         
         for item in q.run(limit=5):
-            result.append({'base': item.base, 'desc': item.desc, 'id_key': str(item.key())})
+            result.append({'base': item.base, 'desc': item.desc, 'id_key': str(item.key().id())})
 
         response.headers['Content-Type'] = 'application/json';
         response.out.write(json.dumps(result));
